0453afe8fbdd70d9d2c7bd4f9f2f413db86d99fe
Mike Morris <michael.patrick.morris@gmail.com>
2014-10-01 18:16:48 -0400

    well, this does things without crashing

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include "view.hpp"
 
 #include <memory>
-#include <stdexcept>
+#include <thread>
 
-int main() {
+void readPixels() {
     uint16_t width = 400;
     uint16_t height = 300;
     float pixelRatio = 1.0;
@@ -17,6 +17,13 @@ int main() {
     const std::unique_ptr<uint32_t[]> pixels(new uint32_t[w * h]);
 
     glReadPixels(0, 0, w, h, GL_RGBA, GL_UNSIGNED_BYTE, pixels.get());
+}
+
+int main() {
+    while (true) {
+        std::thread t(readPixels);
+        t.join();
+    }
 
     return 0;
 }
